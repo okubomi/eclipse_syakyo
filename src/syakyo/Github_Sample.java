c0009d98506893395a0a8932307b167d7e1f6cc3
okubomi <okubomi@gmail.com>
2026-02-17 14:35:54 +0900

    modefy code. 計算処理を追記

--- src/syakyo/Github_Sample.java
+++ src/syakyo/Github_Sample.java
@@ -5,6 +5,9 @@ public class Github_Sample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello Github!!");
+		int x = 2;
+		int y = 8;
+		System.out.println(x + y);
 	}
 
 }
